132ca1ac49d6af0a909cba1e8d44215f4a3d574a
Ace-86 <kronicrespawn@gmail.com>
2023-07-05 21:57:14 -0500

    venv activated

--- server/venv/bin/activate.fish
+++ server/venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ace/Coding/JS Projects/amara/server/venv"
+set -gx VIRTUAL_ENV "/home/ace/Coding/cloned/server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
